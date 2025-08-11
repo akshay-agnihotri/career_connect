9d8c78bbec082f630cdba4863ebfdb626fe5b0f3
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-08-11 21:14:05 +0530

    updated schema.ts

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -2,3 +2,6 @@ export * from "./schema/user";
 export * from "./schema/organization";
 export * from "./schema/jobListing";
 export * from "./schema/jobListingApplication";
+export * from "./schema/userResume";
+export * from "./schema/userNotificationSettings";
+export * from "./schema/organizationUserSetting";
