9b31b9a6b75a4b12b7442fef6ac05628a21a5dc3
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-08-14 16:53:30 +0530

    fixes all the bugs of tailwindcss , shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
- 
+import {
+  Sidebar,
+  SidebarHeader,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
 export default function Home() {
   return (
-    <div></div>
-  )
+    <>
+      <SidebarProvider className="overflow-y-hidden">
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="items-center flex-row!">
+            <SidebarTrigger />
+            <span className="">
+              <span className="text-xl text-nowrap">Carrer-Connect</span>
+            </span>
+          </SidebarHeader>
+        </Sidebar>
+      </SidebarProvider>
+    </>
+  );
 }
