35970ee1a469cbeb3b7b1697599899c066fdd0e6
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-08-20 16:40:45 +0530

    complted navuser sigin-signout

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,9 +1,9 @@
 "use client";
 import {
   IconChevronsDown,
-  IconCreditCard,
-  IconLogout,
-  IconNotification,
+  IconLogin2,
+  IconLogout2,
+  IconSettings,
   IconUserCircle,
 } from "@tabler/icons-react";
 
@@ -24,16 +24,12 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
-import { SignOutButton } from "@clerk/nextjs";
-
-const user = {
-  name: "shadcn",
-  email: "m@example.com",
-  avatar: "/anonymous-avatar.svg",
-};
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
 
 export function NavUser() {
   const { isMobile } = useSidebar();
+  const { isSignedIn, user } = useUser();
 
   return (
     <SidebarMenu>
@@ -44,14 +40,43 @@ export function NavUser() {
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
+              {isSignedIn ? (
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage
+                      src={user?.imageUrl}
+                      alt={user?.firstName ?? "User Avatar"}
+                    />
+                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">
+                      {user?.firstName}
+                    </span>
+                    <span className="truncate text-xs">
+                      {user?.emailAddresses[0]?.emailAddress}
+                    </span>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Image
+                    src={"/anonymous-avatar.svg"}
+                    alt="Anonymous User"
+                    className="h-9 w-9 rounded-lg aspect-square"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">
+                      Anonymous User
+                    </span>
+                    <span className="truncate text-xs">
+                      AnonymousUserEmail@gmail.com
+                    </span>
+                  </div>
+                </div>
+              )}
               <IconChevronsDown className="ml-auto size-4" />
             </SidebarMenuButton>
           </DropdownMenuTrigger>
@@ -62,16 +87,43 @@ export function NavUser() {
             sideOffset={4}
           >
             <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+              {isSignedIn ? (
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage
+                      src={user?.imageUrl}
+                      alt={user?.firstName ?? "User Avatar"}
+                    />
+                    <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">
+                      {user?.firstName}
+                    </span>
+                    <span className="truncate text-xs">
+                      {user?.emailAddresses[0]?.emailAddress}
+                    </span>
+                  </div>
                 </div>
-              </div>
+              ) : (
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Image
+                    src={"/anonymous-avatar.svg"}
+                    alt="Anonymous User"
+                    className="h-9 w-9 rounded-lg aspect-square"
+                    width={30}
+                    height={30}
+                  />
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">
+                      Anonymous User
+                    </span>
+                    <span className="truncate text-xs">
+                      AnonymousUserEmail@gmail.com
+                    </span>
+                  </div>
+                </div>
+              )}
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
@@ -82,22 +134,32 @@ export function NavUser() {
                 </Link>
               </DropdownMenuItem>
               <DropdownMenuItem>
-                <IconCreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <IconNotification />
-                Notifications
+                <Link
+                  href="/user-settings/notifications"
+                  className="flex items-center gap-2"
+                >
+                  <IconSettings />
+                  Settings
+                </Link>
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
-              {/* <IconLogout className="mr-2 h-4 w-4" /> */}
-              <SignOutButton>
-                {/* 
-                  Sign Out
-                */}
-              </SignOutButton>
+              {isSignedIn ? (
+                <SignOutButton>
+                  <div className="w-full flex items-center">
+                    <IconLogout2 className="mr-2 h-4 w-4" />
+                    <span>Sign out</span>
+                  </div>
+                </SignOutButton>
+              ) : (
+                <SignInButton>
+                  <div className="w-full flex items-center">
+                    <IconLogin2 className="mr-2 h-4 w-4" />
+                    <span>Sign in</span>
+                  </div>
+                </SignInButton>
+              )}
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
